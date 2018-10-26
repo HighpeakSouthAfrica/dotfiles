7c75da64222aa42f73d89c11491d00410654e716
Gavin Hill <gaz@#beerygaz.com>
2018-10-26 11:35:23 +0200

    Added sen

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -6,6 +6,7 @@
 
 # Aliases
 	alias v="vim -p"
+	alias sen="docker run --rm --privileged -v /var/run/docker.sock:/run/docker.sock -ti -e TERM tomastomecek/sen"
 	
 	# This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
 	# alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
